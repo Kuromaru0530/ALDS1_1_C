b6ee310ca6264d9e984435b145c76b49a0e21c88
Kuromaru0530 <kuromary77@gmail.com>
2023-04-10 16:39:22 +0900

    プロジェクト ファイルを追加します。

---
+++ ALDS1_1_C/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        var ans = 0;
+        for(int i= 0; i < n; i++)
+        {
+            var In = int.Parse(Console.ReadLine());
+            if (IsPrime(In) == true) ans++;
+        }
+        Console.WriteLine(ans);
+    }
+    static bool IsPrime(int a)
+    {
+        if(a == 2) return true;
+
+        else if(a < 2 || a % 2 == 0) return false;
+
+        var i = 3;
+        while(i <= Math.Sqrt(a))
+        {
+            if (a % i == 0) return false;
+
+            i += 2;
+        }
+        return true;
+    }
+}
